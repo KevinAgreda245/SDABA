7728e1e72955c912f7f9fc6e740e733a8e631bf9
Diego <pcdiego17@gmail.com>
2024-10-24 15:57:32 -0600

    añadiendo log sobre el usuario loggeado

--- app/Http/Controllers/InventarioController.php
+++ app/Http/Controllers/InventarioController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Inventario;
 use App\Models\Producto;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class InventarioController extends Controller
 {
@@ -14,6 +16,8 @@ class InventarioController extends Controller
             ->whereColumn('producto.MINIMO_PRODUCTO', '>=', 'inventario.CANTIDAD_INVENTARIO')
             ->join('producto', 'inventario.ID_PRODUCTO', '=', 'producto.ID_PRODUCTO')
             ->get();
+            $usuario = Auth::user();
+            Log::info($usuario);
         return view('inventario.index', compact('inventarios'));
 
         
